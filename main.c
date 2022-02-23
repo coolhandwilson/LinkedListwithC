2c7694b655b13c51c20f8f512ab3f7991f004afd
Wil Scott <wscott53@my.bcit.ca>
2022-02-23 11:35:03 -0800

    Add main function, define node struct

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+// Created by Wilson Scott on 2022-02-23.
+//
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    int key;
+    struct node *next;
+};
+
+
+
+
+int main() {
+
+    return 0;
+}
