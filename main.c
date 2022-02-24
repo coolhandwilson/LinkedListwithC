7076afb5af6f7fc95d208eb4960a62de084e24ca
Wil Scott <wscott53@my.bcit.ca>
2022-02-23 22:23:20 -0800

    Debug addNode function, test code in main

--- main.c
+++ main.c
@@ -7,28 +7,36 @@
 struct nodes {
     int data;
     int key;
-    struct node *next;
+    struct nodes *next;
 };
 
 struct nodes * addNode (int newData, int newKey) {
-    //create node
-    struct nodes newNode = (struct nodes*)
-    struct nodes newNode = {.data = newData, .key = newKey, .next = NULL};
-    struct nodes* ptr = &newNode;
-    return ptr;
+    //create new node
+    struct nodes *newNode;
+    newNode = (struct nodes*) malloc(sizeof(struct nodes));
+    newNode->data = newData;
+    newNode->key = newKey;
+    newNode->next = NULL;
+    return newNode; // Return is likely unnecessary - need to add loop to sift through links and add that way
 };
 
 
 int main() {
     struct nodes *root;
     root = (struct nodes *) malloc(sizeof(struct nodes));
-    root = {.date = 0, .key = 0, .next = NULL};
+    root->next = NULL;
     root->data = 13;
     root->key = 131;
-    //*root = createNode(1, 1);
 
     printf("%d %d %p", root->data, root->key, &root);
     printf("\n%p", root);
+    //test new node creation
+    root->next = addNode(10, 14);
+
+    printf("\n%d %d %p", root->next->data, root->next->key, &root->next);
+    printf("\n%p", root->next);
+
+    free(root->next);
     free(root);
     return 0;
 }
